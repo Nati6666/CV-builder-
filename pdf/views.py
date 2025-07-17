d2c7550fc23c90fce1674ad732a0d29879232aea
Nati Melkamu <natimelkemu@gmail.com>
2025-07-17 02:33:18 -0700

    added posting to the backend .. post method

--- pdf/views.py
+++ pdf/views.py
@@ -4,4 +4,33 @@ from .models import Profile
 
 # Create your views here.
 def profile(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        summary = request.POST.get('summary')   
+        degree = request.POST.get('degree')
+        experience = request.POST.get('experience')
+        skills = request.POST.get('skills')
+        school = request.POST.get('school')
+        university = request.POST.get('university')
+        achievements = request.POST.get('achievements')
+    
+
+
+        # Save the profile data to the database
+        profile = Profile(
+            name=name,
+            email=email,
+            phone=phone,
+            summary=summary,
+            degree=degree,
+            experience=experience,
+            skills=skills,
+            school=school,
+            university=university,
+            achievements=achievements
+       
+        )
+        profile.save()
     return render(request, 'pdf/accept.html', )
