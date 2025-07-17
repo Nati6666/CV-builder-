fa91285d84578b9de2df4bab611430301996f904
Nati Melkamu <natimelkemu@gmail.com>
2025-07-17 01:28:38 -0700

    building a model

--- pdf/models.py
+++ pdf/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    summary = models.TextField(blank=True, null=True)
+    degree = models.CharField(max_length=100, blank=True, null=True)
+    experience = models.TextField(blank=True, null=True)
+    skills = models.TextField(blank=True, null=True)
+    school = models.CharField(max_length=100, blank=True, null=True)
+    university = models.CharField(max_length=100, blank=True, null=True)
+    achievements = models.TextField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
